39dbcecf1c69ed0b9389c323a2a6dacad7299665
fuhongyi123 <1127941696@qq.com>
2020-08-10 16:32:55 +0800

    test

---
+++ src/main/java/com/sfac/javaSpringBoot/modules/test/conntroller/TestController.java
@@ -0,0 +1,17 @@
+package com.sfac.javaSpringBoot.modules.test.conntroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    /*http://localhost:8080/test/testdesc*/
+    @GetMapping("/testdesc")
+    @ResponseBody
+    public String testDemo(){
+        return "Tis is test module desc";
+    }
+}
